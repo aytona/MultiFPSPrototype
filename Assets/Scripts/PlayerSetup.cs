717de40f0f21ab54de0f2afef52e6c7672ec1a8c
aytona <christopher.aytona@gmail.com>
2015-11-07 21:40:08 -0500

    network/player setup

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    private Behaviour[] componentToDisable;
+    private Camera sceneCamera;
+
+    void Start()
+    {
+        sceneCamera = Camera.main;
+        if (!isLocalPlayer)
+            for (int i = 0; i < componentToDisable.Length; i++)
+                componentToDisable[i].enabled = false;
+        else
+            if (sceneCamera != null)
+                sceneCamera.gameObject.SetActive(false);
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+            sceneCamera.gameObject.SetActive(true);
+    }
+}
